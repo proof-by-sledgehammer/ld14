5927bd5ef0aa6a179fd29f1307f763b9992c5d01
Daniel Spaniol <ds3@ou.nl>
2021-12-11 11:45:51 +0100

    Add energy for building turrets

--- src/scripts/player_controller.gd
+++ src/scripts/player_controller.gd
@@ -7,7 +7,15 @@ enum Player{
 export(Player) var player
 export(float) var moveSpeed = 700.0
 export(NodePath) var turretContainer
+export(NodePath) var energyIndicator
 export(PackedScene) var turretPrefab
+
+var energy = 100.0
+var energyRecoveryRate = 10.0
+
+const TURRET1_COST = 30.0
+const TURRET2_COST = 50.0
+const TURRET3_COST = 70.0
 
 func getXInput():
 	match player:
@@ -23,29 +31,50 @@ func getYInput():
 		Player.PLAYER2:
 			return Input.get_action_strength("player2_move_down") - Input.get_action_strength("player2_move_up")	
 
-func spawn_turret():
+func turret_cost(turretType):
+	match turretType:
+		Turret.Turret.TURRET1:
+			return TURRET1_COST
+		Turret.Turret.TURRET2:
+			return TURRET2_COST
+		Turret.Turret.TURRET3:
+			return TURRET3_COST
+
+func spawn_turret(turretType):
 	var turret = turretPrefab.instance()
 	turret.position = position
 	get_node(turretContainer).add_child(turret)
+	energy = energy - turret_cost(turretType)
 
 func process_turret_input():
 	match player:
 		Player.PLAYER1:
-			if Input.is_action_just_pressed("player1_place_turret1"):
-				spawn_turret()
-			if Input.is_action_just_pressed("player1_place_turret2"):
-				spawn_turret()
-			if Input.is_action_just_pressed("player1_place_turret3"):
-				spawn_turret()
+			if Input.is_action_just_pressed("player1_place_turret1") and energy >= TURRET1_COST:
+				spawn_turret(Turret.Turret.TURRET1)
+			if Input.is_action_just_pressed("player1_place_turret2") and energy >= TURRET2_COST:
+				spawn_turret(Turret.Turret.TURRET2)
+			if Input.is_action_just_pressed("player1_place_turret3") and energy >= TURRET3_COST:
+				spawn_turret(Turret.Turret.TURRET3)
 		Player.PLAYER2:
-			if Input.is_action_just_pressed("player2_place_turret1"):
-				spawn_turret()
-			if Input.is_action_just_pressed("player2_place_turret2"):
-				spawn_turret()
-			if Input.is_action_just_pressed("player2_place_turret3"):
-				spawn_turret()
+			if Input.is_action_just_pressed("player2_place_turret1") and energy >= TURRET1_COST:
+				spawn_turret(Turret.Turret.TURRET1)
+			if Input.is_action_just_pressed("player2_place_turret2") and energy >= TURRET2_COST:
+				spawn_turret(Turret.Turret.TURRET2)
+			if Input.is_action_just_pressed("player2_place_turret3") and energy >= TURRET3_COST:
+				spawn_turret(Turret.Turret.TURRET3)
+
+func update_energy():
+	get_node(energyIndicator).value = energy
+	
+func recover_energy(delta):
+	if energy < 100:
+		energy = energy + delta * energyRecoveryRate
+	else:
+		energy = 100
 
 func _process(delta):
 	position.x += getXInput() * moveSpeed * delta
 	position.y += getYInput() * moveSpeed * delta
+	recover_energy(delta)
 	process_turret_input()
+	update_energy()
---
+++ src/scripts/turret.gd
@@ -0,0 +1,8 @@
+class_name Turret
+extends Node2D
+
+enum Turret {
+	TURRET1,
+	TURRET2,
+	TURRET3
+}
