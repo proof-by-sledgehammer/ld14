38a3b79aae2f8289afe3cea412093b204f16d0f7
Daniel Spaniol <ds3@ou.nl>
2021-12-11 11:29:04 +0100

    Player 2 can spawn turrets

--- src/scripts/player_controller.gd
+++ src/scripts/player_controller.gd
@@ -6,6 +6,8 @@ enum Player{
 
 export(Player) var player
 export(float) var moveSpeed = 700.0
+export(NodePath) var turretContainer
+export(PackedScene) var turretPrefab
 
 func getXInput():
 	match player:
@@ -21,6 +23,29 @@ func getYInput():
 		Player.PLAYER2:
 			return Input.get_action_strength("player2_move_down") - Input.get_action_strength("player2_move_up")	
 
+func spawn_turret():
+	var turret = turretPrefab.instance()
+	turret.position = position
+	get_node(turretContainer).add_child(turret)
+
+func process_turret_input():
+	match player:
+		Player.PLAYER1:
+			if Input.is_action_just_pressed("player1_place_turret1"):
+				spawn_turret()
+			if Input.is_action_just_pressed("player1_place_turret2"):
+				spawn_turret()
+			if Input.is_action_just_pressed("player1_place_turret3"):
+				spawn_turret()
+		Player.PLAYER2:
+			if Input.is_action_just_pressed("player2_place_turret1"):
+				spawn_turret()
+			if Input.is_action_just_pressed("player2_place_turret2"):
+				spawn_turret()
+			if Input.is_action_just_pressed("player2_place_turret3"):
+				spawn_turret()
+
 func _process(delta):
 	position.x += getXInput() * moveSpeed * delta
 	position.y += getYInput() * moveSpeed * delta
+	process_turret_input()
