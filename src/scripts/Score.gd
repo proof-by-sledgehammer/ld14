120f69f4a95c406eb2b89149d09d2032674ff18a
Daniel Spaniol <ds3@ou.nl>
2021-12-11 16:40:21 +0100

    GUI stuff

---
+++ src/scripts/Score.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+export(int) var player1_score = 0
+export(int) var player2_score = 0
+
+export(NodePath) var player1_score_text
+export(NodePath) var player2_score_text
+
+func _process(delta):
+	get_node(player1_score_text).text = player1_score as String
+	get_node(player2_score_text).text = player2_score as String
